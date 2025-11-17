9e3fedd86ab1e2ce33cf8a51aacfe999f8d65ae2
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-11-17 11:12:34 +0530

    Add Category and Product models with slug support

---
+++ apps/core/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
+
+class Category(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    slug = models.SlugField(max_length=150, unique=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base = self.name
+            self.slug = slugify(base)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('core:product_detail', args=[self.slug])
+
+    def __str__(self):
+        return self.name
