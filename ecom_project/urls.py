c367e5757271a06828270b518dce5736c006d210
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-11-17 11:06:25 +0530

    Add URL routing for the e-commerce project

---
+++ ecom_project/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('apps.core.urls')),
+    path('accounts/', include('apps.accounts.urls')),
+    path('cart/', include('apps.cart.urls')),
+    path('orders/', include('apps.orders.urls')),
+    path('api/', include('apps.api.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
